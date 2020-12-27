9fbf17b58483031c648028a32e4fce6d12887117
phella <philopateernabil2021@gmail.com>
2020-12-27 10:04:10 +0200

    add sign up comonent

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup , FormBuilder, Validators} from '@angular/forms';
+import { Router } from "@angular/router";
+
+import { RequestService } from '../request.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  loginForm: FormGroup;
+  wrong: Boolean;
+
+  constructor(private fb: FormBuilder, private request: RequestService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username : ['', Validators.required],
+      password : ['', Validators.required]
+      });
+
+  }
+
+  submit():void {
+    this.request.login(this.loginForm.value).subscribe( res => {
+      if(res)
+        this.router.navigate(['home']);
+      else
+        this.wrong = true;;   
+    });
+  }
+}
