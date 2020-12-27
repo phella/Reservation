784d109c46226b965c88ec108c78efae2dadee4c
phella <philopateernabil2021@gmail.com>
2020-12-27 17:09:31 +0200

    add match model

---
+++ src/app/models/match.ts
@@ -0,0 +1,9 @@
+export interface Match {
+    home_team: string;
+    away_team: string;
+    match_venue: string;
+    date: Date;
+    main_referee: string;
+    line_man1: string;
+    line_man2: string;
+}
